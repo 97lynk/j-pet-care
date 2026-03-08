b5de1c0fed6e943f36f2ee47dc5621afbdc5823e
Tuan Nguyen <97lynk@gmail.com>
2026-03-08 11:18:49 +0700

    feat(app): refactor code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
-import {routes} from './app-routing.module';
+import {provideRouter, Routes} from '@angular/router';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideTranslateService} from '@ngx-translate/core';
 import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
@@ -9,6 +8,37 @@ import {mockHttpInterceptor} from './services/mock-http-interceptor';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 import {MAT_DATE_LOCALE} from '@angular/material/core';
 import {provideAnimations} from '@angular/platform-browser/animations';
+import {MyRegistrationsComponent} from "./components/my-registrations/my-registrations.component";
+import {CancelRegistrationComponent} from "./components/cancel-registration/cancel-registration.component";
+import {
+  RegisterForVaccinationComponent
+} from "./components/register-for-vaccination/register-for-vaccination.component";
+import {HomeComponent} from "./components/home/home.component";
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    component: RegisterForVaccinationComponent,
+  },
+  {
+    path: 'cancel',
+    component: CancelRegistrationComponent,
+  },
+  {
+    path: 'my-registrations',
+    component: MyRegistrationsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
